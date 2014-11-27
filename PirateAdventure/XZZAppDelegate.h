d1e4a8a723c8d72aa6ba7b7fe40d9f34765e1168
zac1st1k <zacfirst@gmail.com>
2014-11-27 20:20:53 +1100

    Initial Commit

---
+++ PirateAdventure/XZZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XZZAppDelegate.h
+//  PirateAdventure
+//
+//  Created by Zac on 27/11/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XZZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PirateAdventureTests/PirateAdventureTests.m
@@ -0,0 +1,34 @@
+//
+//  PirateAdventureTests.m
+//  PirateAdventureTests
+//
+//  Created by Zac on 27/11/2014.
+//  Copyright (c) 2014 1st1k. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PirateAdventureTests : XCTestCase
+
+@end
+
+@implementation PirateAdventureTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
