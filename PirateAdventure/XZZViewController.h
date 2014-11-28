216b76ec64f85be6dbaaff1ee2cbe3e48e401a94
zac1st1k <zacfirst@gmail.com>
2014-11-28 17:50:29 +1100

    + Tiles and Navigation

--- PirateAdventure/XZZViewController.h
+++ PirateAdventure/XZZViewController.h
@@ -11,7 +11,8 @@
 @interface XZZViewController : UIViewController
 
 //iVars
-@property (nonatomic) CGPoint 
+@property (nonatomic) CGPoint currentPoint;
+@property (strong, nonatomic) NSArray *tiles;
 
 //IBOutlets
 @property (strong, nonatomic) IBOutlet UIImageView *backgroundImageView;
@@ -19,9 +20,12 @@
 @property (strong, nonatomic) IBOutlet UILabel *healthLabel;
 @property (strong, nonatomic) IBOutlet UILabel *damageLabel;
 @property (strong, nonatomic) IBOutlet UILabel *weaponLabel;
+@property (strong, nonatomic) IBOutlet UILabel *storyLabel;
 @property (strong, nonatomic) IBOutlet UIButton *actionButton;
+
 @property (strong, nonatomic) IBOutlet UIButton *southButton;
-@property (strong, nonatomic) IBOutlet UIButton *weatButton;
+@property (strong, nonatomic) IBOutlet UIButton *westButton;
+
 @property (strong, nonatomic) IBOutlet UIButton *northButton;
 @property (strong, nonatomic) IBOutlet UIButton *eastButton;
 
--- PirateAdventure/XZZViewController.m
+++ PirateAdventure/XZZViewController.m
@@ -8,6 +8,7 @@
 
 #import "XZZViewController.h"
 #import "XZZFactory.h"
+#import "XZZTile.h"
 
 @interface XZZViewController ()
 
@@ -20,12 +21,10 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     XZZFactory *factory = [[XZZFactory alloc] init];
-    NSArray *tiles = [factory tiles];
-    NSLog(@"%@",tiles);
-    
-    
-    
-//    CGPoint myPoint = CGPointMake(3, 3);
+    self.tiles = [factory tiles];
+    self.currentPoint = CGPointMake(0, 0);
+    [self updateTile];
+    [self updateButtons];
 //    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"Alert!" message:@"Hello!" delegate:nil cancelButtonTitle:@"Ok" otherButtonTitles: nil];
 //    [alertView show];
     
@@ -41,15 +40,56 @@
 - (IBAction)actionButtonPressed:(UIButton *)sender {
 }
 
+
+- (IBAction)northButtonPressed:(UIButton *)sender {
+    self.currentPoint = CGPointMake(self.currentPoint.x, self.currentPoint.y + 1);
+    [self updateButtons];
+    [self updateTile];
+}
+
 - (IBAction)southButtonPressed:(UIButton *)sender {
+    self.currentPoint = CGPointMake(self.currentPoint.x, self.currentPoint.y - 1);
+    [self updateButtons];
+    [self updateTile];
+
 }
 
 - (IBAction)westButtonPressed:(UIButton *)sender {
-}
+    self.currentPoint = CGPointMake(self.currentPoint.x - 1, self.currentPoint.y);
+    [self updateButtons];
+    [self updateTile];
 
-- (IBAction)northButtonPressed:(UIButton *)sender {
 }
 
 - (IBAction)eastButtonPressed:(UIButton *)sender {
+    self.currentPoint = CGPointMake(self.currentPoint.x + 1, self.currentPoint.y);
+    [self updateButtons];
+    [self updateTile];
+
 }
+
+
+- (void)updateTile
+{
+    XZZTile * tileModel = [[self.tiles objectAtIndex:self.currentPoint.x] objectAtIndex:self.currentPoint.y];
+    self.storyLabel.text = tileModel.story;
+}
+
+- (void)updateButtons
+{
+    self.westButton.hidden = [self tileExistsAtPoint:CGPointMake(self.currentPoint.x - 1, self.currentPoint.y)];
+    self.eastButton.hidden = [self tileExistsAtPoint:CGPointMake(self.currentPoint.x + 1, self.currentPoint.y)];
+    self.northButton.hidden = [self tileExistsAtPoint:CGPointMake(self.currentPoint.x, self.currentPoint.y + 1)];
+    self.southButton.hidden = [self tileExistsAtPoint:CGPointMake(self.currentPoint.x, self.currentPoint.y - 1)];
+}
+
+- (BOOL)tileExistsAtPoint:(CGPoint)point
+{
+    if (point.y >= 0 && point.x >= 0 && point.x < [self.tiles count] && point.y < [[self.tiles objectAtIndex:point.x] count]){
+        return NO;
+    }
+    else
+        return YES;
+}
+
 @end
